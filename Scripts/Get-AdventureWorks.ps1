ccf11acc3f8805557cef589f5d703800b7f01154
Eric Cote <eric@coteexpert.com>
2016-12-03 05:18:08 +0000

    get adventture works correction

--- Scripts/Get-AdventureWorks.ps1
+++ Scripts/Get-AdventureWorks.ps1
@@ -1,5 +1,27 @@
+[CmdletBinding()]
+Param
+    ( 
+    [string]$Destination = $(Join-Path  $env:USERPROFILE "downloads\"),
+    [switch] $Uninstall = $false ,
+    [switch] $Aw = $true, 
+    [switch] $AwDw = $false, 
+    [switch] $AwLt = $true, 
+    [switch] $Wwi = $false,
+    [switch] $WwiDw = $false  
+    )
 
-$dl=$env:USERPROFILE + "\downloads\"
+
+if($Uninstall)
+{
+    $Aw = $false; 
+    $AwDw = $false; 
+    $AwLt = $false;
+    $Wwi = $false;
+    $WwiDw = $false;  
+}
+
+$dl=$Destination
+
 
 function detect-localdb 
 { 
@@ -107,188 +129,210 @@ if (Get-ServerName -neq '')
     #get codeplex-Version
     $codeplexVersion= Get-CodeplexVersion
      
-    New-Item -type directory -path C:\aw
+    New-Item -type directory -path C:\aw -InformationAction SilentlyContinue -ErrorAction SilentlyContinue
     $Acl = Get-Acl "C:\aw"
     $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("BUILTIN\Users","FullControl","ContainerInherit,ObjectInherit","None","Allow")
     $Acl.AddAccessRule($ar)
     Set-Acl "C:\aw" $Acl
 
-    ###------------------------------------------------------
 
 
-    "Downloading AdventureWorks 2014..."
-    $FileNameAW2014=Join-path $dl  "Adventure Works 2014 Full Database Backup.zip"
-    Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880661&FileTime=130507138100830000&Build=$codeplexVersion" $FileNameAW2014
 
-    add-type -AssemblyName System.IO.Compression.FileSystem
-    [system.io.compression.zipFile]::ExtractToDirectory($FileNameAW2014,'c:\aw\')
 
-    "Installing AdventureWorks 2014..."
-    $cmd="
-    RESTORE DATABASE AdventureWorks2014
-      FROM DISK = 'C:\AW\AdventureWorks2014.bak'
-    WITH   
-      MOVE 'AdventureWorks2014_Data' 
-      TO 'C:\AW\AdventureWorks_data.mdf', 
-      MOVE 'AdventureWorks2014_Log' 
-      TO 'C:\AW\AdventureWorks_log.ldf';
-    GO
+    ###-------------------------------------------------------------------------------
+
+    if($AwLt)
+    {
+        "Downloading AdventureWorks LT 2012..."
+
+        $FileNameAWLT2012 = Join-path $dl "AdventureWorksLT2012_Data.mdf";
+        Download-File  "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=354847&FileTime=129764108568330000&Build=$codeplexVersion" $FileNameAWLT2012
+        Copy-Item  -Path $FileNameAWLT2012 -Destination 'c:\aw\'
+
+
+        "Installing AdventureWorks LT 2012..."
+        $cmd="
+        CREATE DATABASE AdventureWorksLT2012 ON 
+        ( FILENAME = N'C:\aw\AdventureWorksLT2012_Data.mdf' )
+         FOR ATTACH_REBUILD_LOG  ;
+        GO
+        ALTER AUTHORIZATION ON DATABASE::AdventureWorksLT2012 TO sa;
+        "
+
+        run-sql $cmd
+        del $FileNameAWLT2012 -ErrorAction SilentlyContinue
     
-    ALTER AUTHORIZATION ON DATABASE::AdventureWorks2014 TO sa;
-    "
+    }
 
-    run-sql $cmd
 
+
+    ###------------------------------------------------------
+    if ($Aw)
+    {
+        "Downloading AdventureWorks 2014..."
+        $FileNameAW2014=Join-path $dl  "Adventure Works 2014 Full Database Backup.zip"
+        Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880661&FileTime=130507138100830000&Build=$codeplexVersion" $FileNameAW2014
+
+        add-type -AssemblyName System.IO.Compression.FileSystem
+        [system.io.compression.zipFile]::ExtractToDirectory($FileNameAW2014,'c:\aw\')
+
+        "Installing AdventureWorks 2014..."
+        $cmd="
+        RESTORE DATABASE AdventureWorks2014
+          FROM DISK = 'C:\AW\AdventureWorks2014.bak'
+        WITH   
+          MOVE 'AdventureWorks2014_Data' 
+          TO 'C:\AW\AdventureWorks_data.mdf', 
+          MOVE 'AdventureWorks2014_Log' 
+          TO 'C:\AW\AdventureWorks_log.ldf';
+        GO
+    
+        ALTER AUTHORIZATION ON DATABASE::AdventureWorks2014 TO sa;
+        "
+
+        run-sql $cmd
+    
+        del $FileNameAW2014 -ErrorAction SilentlyContinue
+    }
 
     ###----------------------------------------------------
 
-
-    "Downloading AdventureWorks DW 2014..."
-    $FileNameAWDW2014=Join-path $dl  "Adventure Works DW 2014 Full Database Backup.zip"
-
-    Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880664&FileTime=130511246406570000&Build=$codeplexVersion" $FileNameAWDW2014
-
-
-    add-type -AssemblyName System.IO.Compression.FileSystem
-    [system.io.compression.zipFile]::ExtractToDirectory($FileNameAWDW2014,'c:\aw\')
-
-
-    "Installing AdventureWorks DW 2014..."
-
-    $cmd="
-    RESTORE DATABASE AdventureWorksDW2014
-      FROM DISK = 'C:\AW\AdventureWorksDW2014.bak'
-    WITH   
-      MOVE 'AdventureWorksDW2014_Data' 
-      TO 'C:\AW\AdventureWorksDW_data.mdf', 
-      MOVE 'AdventureWorksDW2014_Log' 
-      TO 'C:\AW\AdventureWorksDW_log.ldf';
-    GO
-    
-    ALTER AUTHORIZATION ON DATABASE::AdventureWorksDW2014 TO sa;
-    "
-
-    run-sql $cmd
-
-
-    ###-------------------------------------------------------------------------------
-
-
-    "Downloading AdventureWorks LT 2012..."
-
-    $FileNameAWLT2012 = Join-path $dl "AdventureWorksLT2012_Data.mdf";
-    Download-File  "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=354847&FileTime=129764108568330000&Build=$codeplexVersion" $FileNameAWLT2012
-    Copy-Item  -Path $FileNameAWLT2012 -Destination 'c:\aw\'
-
-
-    "Installing AdventureWorks LT 2012..."
-    $cmd="
-    CREATE DATABASE AdventureWorksLT2012 ON 
-    ( FILENAME = N'C:\aw\AdventureWorksLT2012_Data.mdf' )
-     FOR ATTACH_REBUILD_LOG  ;
-    GO
-    ALTER AUTHORIZATION ON DATABASE::AdventureWorksLT2012 TO sa;
-    "
-
-    run-sql $cmd
-
-
-    ###-------------------------------------------------------------------------------
-
-
-    if (get-sqlYear -ge 2018)
+    If($AwDw)
     {
-     # $SqlFeature="Standard"
-     # if(("Enterprise","Developer") -contains (Get-SqlEdition))
-     # { $SqlFeature="Full" }
+        "Downloading AdventureWorks DW 2014..."
+        $FileNameAWDW2014=Join-path $dl  "Adventure Works DW 2014 Full Database Backup.zip"
+
+        Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=880664&FileTime=130511246406570000&Build=$codeplexVersion" $FileNameAWDW2014
+
+
+        add-type -AssemblyName System.IO.Compression.FileSystem
+        [system.io.compression.zipFile]::ExtractToDirectory($FileNameAWDW2014,'c:\aw\')
+
+
+        "Installing AdventureWorks DW 2014..."
+
+        $cmd="
+        RESTORE DATABASE AdventureWorksDW2014
+          FROM DISK = 'C:\AW\AdventureWorksDW2014.bak'
+        WITH   
+          MOVE 'AdventureWorksDW2014_Data' 
+          TO 'C:\AW\AdventureWorksDW_data.mdf', 
+          MOVE 'AdventureWorksDW2014_Log' 
+          TO 'C:\AW\AdventureWorksDW_log.ldf';
+        GO
+    
+        ALTER AUTHORIZATION ON DATABASE::AdventureWorksDW2014 TO sa;
+        "
+
+        run-sql $cmd
+
+        del $FileNameAWDW2014 -ErrorAction SilentlyContinue
+    }
+
+
+    ###-------------------------------------------------------------------------------
+    # Code to detect if we are using the full version of wwi (with in memory databases)
+    # or the Standard version.
+   
+   
     $SqlFeature="Full"
     if ((Get-ServerName) -eq '(localdb)\MSSQLLocalDB')
     {
         $SqlFeature="Standard"
     }
+    #pre-sp1 version could only do in-memory databases with enterprise or dev edition
+    # $SqlFeature="Standard"
+    # if(("Enterprise","Developer") -contains (Get-SqlEdition))
+    # { $SqlFeature="Full" }
    
-    
-    "Downloading Wide World Importers..."
-    Download-File ("https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-" + $SqlFeature + ".bak")  (Join-path $dl  ("WideWorldImporters-$SqlFeature.bak"))
+    if ($Wwi)
+    {
+        "Downloading Wide World Importers..."
+        Download-File ("https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-" + $SqlFeature + ".bak")  (Join-path $dl  ("WideWorldImporters-$SqlFeature.bak"))
 
-    Copy-Item  -Path (Join-path $dl  "WideWorldImporters-*.bak") -Destination 'c:\aw\'
+        Copy-Item  -Path (Join-path $dl  "WideWorldImporters-*.bak") -Destination 'c:\aw\'
 
-    "Installing Wide World Importers..."
-    $part=""
-    if ($SqlFeature -eq "Full") 
-    {  $part = " MOVE 'WWI_InMemory_Data_1' TO 'c:\AW\WideWorldImporters_InMemory_Data_1', " };
+        "Installing Wide World Importers..."
+        $part=""
+        if ($SqlFeature -eq "Full") 
+        {  $part = " MOVE 'WWI_InMemory_Data_1' TO 'c:\AW\WideWorldImporters_InMemory_Data_1', " };
 
-    $cmd="
-    RESTORE DATABASE WideWorldImporters
-      FROM DISK = 'C:\AW\WideWorldImporters-$SqlFeature.bak'
-    WITH   
-      MOVE 'WWI_Primary' 
-      TO 'C:\AW\WideWorldImporters.mdf', 
-      MOVE 'WWI_UserData' 
-      TO 'C:\AW\WideWorldImporters_UserData.ndf',
-       $part
-      MOVE 'WWI_Log' 
-      TO 'C:\AW\WideWorldImporters.ldf';
-    GO
-    ALTER AUTHORIZATION ON DATABASE::WideWorldImporters TO sa;
-    "
-
-
-    Run-Sql $cmd
+        $cmd="
+        RESTORE DATABASE WideWorldImporters
+          FROM DISK = 'C:\AW\WideWorldImporters-$SqlFeature.bak'
+        WITH   
+          MOVE 'WWI_Primary' 
+          TO 'C:\AW\WideWorldImporters.mdf', 
+          MOVE 'WWI_UserData' 
+          TO 'C:\AW\WideWorldImporters_UserData.ndf',
+           $part
+          MOVE 'WWI_Log' 
+          TO 'C:\AW\WideWorldImporters.ldf';
+        GO
+        ALTER AUTHORIZATION ON DATABASE::WideWorldImporters TO sa;
+        "
 
 
+        Run-Sql $cmd
+
+        del (Join-path $dl  "WideWorldImporters*.bak") -ErrorAction SilentlyContinue
+    }
 
     ###-------------------------------------------------------------------------------
 
-    "Downloading Wide World Importers..."
-    Download-File "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-$SqlFeature.bak" (Join-path $dl "WideWorldImportersDW-$SqlFeature.bak")
+
+    if($WwiDw)
+    {
+        "Downloading Wide World Importers..."
+        Download-File "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-$SqlFeature.bak" (Join-path $dl "WideWorldImportersDW-$SqlFeature.bak")
 
 
-    Copy-Item  -Path (Join-path $dl  "WideWorldImportersDW-*.bak") -Destination 'c:\aw\'
+        Copy-Item  -Path (Join-path $dl  "WideWorldImportersDW-*.bak") -Destination 'c:\aw\'
 
 
-    "Installing Wide World Importers..."
-    $part=""
-    if ($SqlFeature -eq "Full") 
-    {  $part =  " MOVE 'WWIDW_InMemory_Data_1' TO 'c:\AW\WideWorldImportersDW_InMemory_Data_1', "  };
+        "Installing Wide World Importers..."
+        $part=""
+        if ($SqlFeature -eq "Full") 
+        {  $part =  " MOVE 'WWIDW_InMemory_Data_1' TO 'c:\AW\WideWorldImportersDW_InMemory_Data_1', "  };
 
 
-    $cmd="
-    RESTORE DATABASE WideWorldImportersDW
-      FROM DISK = 'C:\AW\WideWorldImportersDW-$SqlFeature.bak'
-    WITH   
-      MOVE 'WWI_Primary' 
-      TO 'C:\AW\WideWorldImportersDW.mdf', 
-      MOVE 'WWI_UserData' 
-      TO 'C:\AW\WideWorldImportersDW_UserData.ndf',
-      $part 
-      MOVE 'WWI_Log' 
-      TO 'C:\AW\WideWorldImportersDW.ldf';
-    GO
-    ALTER AUTHORIZATION ON DATABASE::WideWorldImportersDW TO sa;
-    "
+        $cmd="
+        RESTORE DATABASE WideWorldImportersDW
+          FROM DISK = 'C:\AW\WideWorldImportersDW-$SqlFeature.bak'
+        WITH   
+          MOVE 'WWI_Primary' 
+          TO 'C:\AW\WideWorldImportersDW.mdf', 
+          MOVE 'WWI_UserData' 
+          TO 'C:\AW\WideWorldImportersDW_UserData.ndf',
+          $part 
+          MOVE 'WWI_Log' 
+          TO 'C:\AW\WideWorldImportersDW.ldf';
+        GO
+        ALTER AUTHORIZATION ON DATABASE::WideWorldImportersDW TO sa;
+        "
 
-    run-sql $cmd
+        run-sql $cmd
 
+        del (Join-path $dl  "WideWorldImporters*.bak") -ErrorAction SilentlyContinue
     }
 
-    #####---------------------------------------------------------------------------------------------
 
-    del $FileNameAW2014
-    del $FileNameAWDW2014
-    del $FileNameAWLT2012
-    del (Join-path $dl  "WideWorldImporters*.bak") 
 }
 else
 {
     "No SQL Server detected!"
 }
 
-if ($uninstall -eq $true)
+if ($Uninstall)
 {
-    run-sql "DROP DATABASE WideWorldImportersDW;"
-    run-sql "DROP DATABASE WideWorldImporters;"
-    run-sql "DROP DATABASE AdventureWorksLT2012;"
-    run-sql "DROP DATABASE AdventureWorksDW2014;"
-    run-sql "DROP DATABASE AdventureWorks2014;"
+    run-sql @"
+      DROP DATABASE WideWorldImportersDW;
+      DROP DATABASE WideWorldImporters;
+      DROP DATABASE AdventureWorksLT2012;
+      DROP DATABASE AdventureWorksDW2014;
+      DROP DATABASE AdventureWorks2014;
+"@
+    
+    rd c:\aw -Recurse 
+
 }
