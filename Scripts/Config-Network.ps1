0c546490a9471211b34994e79e59d13f691d2c1b
Eric Cote <eric@coteexpert.com>
2018-02-02 10:09:49 -0500

    restore right snapshot and use bootstrap

--- Scripts/Config-Network.ps1
+++ Scripts/Config-Network.ps1
@@ -12,13 +12,13 @@ $hex2 = '{0:x2}' -f $d.Minute;
 $hex3 = '{0:x2}' -f $d.Second;
 $hex4 = '{0:x2}' -f ($d.Millisecond % 256);
 $hex5 = '{0:x2}' -f ($ip4 -as [int]);
-　
+
 $macAddressStart =$hex1+$hex2+$hex3+$hex4+$hex5+'00';
 $macAddressEnd =$hex1+$hex2+$hex3+$hex4+$hex5+'ff';
-　
 
-　
 Set-VMHost -MacAddressMinimum $macAddressStart   -MacAddressMaximum $macAddressEnd
+
+Restore-VMSnapshot -Name "Config Reseau 172.16.0.1-255.255.0.0" -VMName "MS-Gate" -Confirm:$false
 
 #Get-VMNetworkAdapter -vmName "MS-GATE" | `
 # where SwitchName -EQ "External Network" | `
